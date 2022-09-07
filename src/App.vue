ad12688fd1625506e60570130672479e5bf67360
jamiromic <jamiromic@gmail.com>
2022-09-07 13:09:18 +0200

    init_and_add_header

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <HeaderComponent/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+}
+}
+</script>
+
+<style lang="scss">
+@import "./styles/index.scss";
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <img src="https://cdn-icons-png.flaticon.com/512/2111/2111624.png" alt="Spotify Icon">
+  </header>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
+<style scoped lang="scss">
+
+  header {
+
+    background-color: #2E3A46;
+    padding: 0.85rem 1.70rem;
+
+    img {
+      width: 50px;
+    }
+  }
+
+</style>
